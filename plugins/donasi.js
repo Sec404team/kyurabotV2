0caac87e1278a221d352ba601fa658024a1e4ace
X-SecTeam <88809651+Sec404team@users.noreply.github.com>
2021-12-20 19:35:01 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ╭─˗ˏˋ*Donasi • Emoney - Pulsa* ´ˎ˗
-│ ✎ PULSA : 081998903280
-│ ✎ Dana/Ovo/Gopay : 081998903280
-│ ✎ Bila Perlu hubungi owner 
+│ ✎ PULSA : 085706642632
+│ ✎ Dana/Gopay : 085706642632
+│ ✎ Bila Perlu hubungi owner
 ╰‿‿‿‿‿‿‿‿‿‿‿‿‿‿
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
