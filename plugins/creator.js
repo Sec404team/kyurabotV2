769612f484928df4c70ce201c26e5dff6cf56d97
X-SecTeam <88809651+Sec404team@users.noreply.github.com>
2021-12-23 13:11:05 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281998903280', 'Kyura', m)
+  this.sendContact(m.chat, '6285706642632', 'HafARsyd', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
